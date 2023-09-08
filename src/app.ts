b0717cb94b040e4ac1010071a588ad784f15b546
autsada <autsada.t@gmail.com>
2023-09-08 09:25:11 +0700

    removed testing route

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ import http from "http"
 import "./firebase/config"
 import * as router from "./routes"
 import { errorHandler } from "./middlewares/error"
-import { createIdTokenfromCustomToken } from "./firebase/helpers"
 
 const { PORT } = process.env
 const port = Number(PORT || 8000)
@@ -17,23 +16,6 @@ const app = express()
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cors())
-
-/**
- * This route is for testing only
- */
-// =============
-app.get("/id-token", async (req, res, next) => {
-  try {
-    const { uid } = req.body as { uid: string }
-    const result = await createIdTokenfromCustomToken(uid)
-
-    res.status(200).json({ token: result })
-  } catch (error) {
-    console.log("error: ", error)
-    next(error)
-  }
-})
-// =============
 
 app.use("/wallet", router.walletRouter)
 app.use("/auth", router.authRouter)
