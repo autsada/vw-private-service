e8dddbdbfc73e7daa2042ceb16a2c62b4db5fd4b
autsada <autsada.t@gmail.com>
2023-09-28 16:13:15 +0700

    updated event logs

--- src/lib/tips.ts
+++ src/lib/tips.ts
@@ -85,17 +85,12 @@ export async function sendTips(input: SendTipsInput) {
     value: tips,
   })
   const txnRct = await txn.wait()
-  console.log("txnRct -->", txnRct)
-  const txnHash = txnRct?.hash
+  // console.log("txnRct -->", txnRct)
+  // const txnHash = txnRct?.hash
 
-  console.log("logs 1 -->", txnRct?.logs)
-  let event: ethers.EventLog | undefined = undefined
-  if (txnHash) {
-    const eventLogs = await contract.queryTransaction(txnHash)
-    console.log("logs 2 -->", eventLogs)
-    event = eventLogs.find((e) => e.eventName === "TipsTransferred")
-    console.log("event -->", event)
-  }
+  // console.log("logs 1 -->", txnRct?.logs)
+  let event = txnRct?.logs[0] as ethers.EventLog
+  console.log("event -->", event)
 
   if (!event) return null
 
